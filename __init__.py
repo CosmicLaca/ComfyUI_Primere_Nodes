a43227fb614f84755b9c66cb23bdcd5c5a3e87f9
Primere Development <gitarfogas@gmail.com>
2024-03-30 10:43:04 +0100

    V 0.5.6 - Visual modal extended file support

--- __init__.py
+++ __init__.py
@@ -28,7 +28,7 @@ is_frontend_symlinked = False
 
 ClientTime = str(datetime.strptime(str(datetime.now(pytz.timezone('GMT0'))), '%Y-%m-%d %H:%M:%S.%f+00:00'))
 # ClientTime = time.gmtime()
-UpdateRequired = '2024-03-24 14:00:00'
+UpdateRequired = '2024-03-30 09:00:00'
 
 if os.path.isdir(frontend_target) == True:
     try:
