32b207efcd9a9071171bfbd71e1da520229d75e8
DESKTOP-CNFQ7PM\Primere <gitarfogas@gmail.com>
2024-11-17 17:57:34 +0100

    V 1.0.0 - model terminal helper

--- terminal_helpers/model_version_cache.py
+++ terminal_helpers/model_version_cache.py
@@ -43,7 +43,8 @@ if len(ModelsList) > 0:
             if model_version is None or model_version not in utility.SUPPORTED_MODELS:
                 File_link = Path(str(model_path)).resolve()
                 comfyModelDir = str(Path(folder_paths.folder_names_and_paths['checkpoints'][0][0]).parent)
-                modelType = str(File_link)[(len(comfyModelDir) + 1):(str(File_link).find('\\', len(comfyModelDir) + 1))]
+                # modelType = str(File_link)[(len(comfyModelDir) + 1):(str(File_link).find('\\', len(comfyModelDir) + 1))]
+                modelType = str(File_link)[str(File_link).index(os.sep + Path(comfyModelDir).stem) + len(Path(comfyModelDir).stem) + 2:(str(File_link).find(os.sep, len(comfyModelDir) + 1))]
                 utility.add_value_to_cache('model_version', modelaname_only, f"{modelType}_symlink")
                 print(f"Model [{model_counter}] / {len(ModelsList)} symlinked file: {modelaname_only} -> from: {modelType}")
             else:
