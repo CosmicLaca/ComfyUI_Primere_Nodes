aa10adce6058d79532219bcba8c76283d268b8d0
DESKTOP-CNFQ7PM\Primere <gitarfogas@gmail.com>
2025-01-05 18:18:14 +0100

    V 1.0.7 - ver

--- __init__.py
+++ __init__.py
@@ -13,7 +13,7 @@ from .Nodes import Networks
 from .Nodes import Segments
 import shutil
 
-__version__ = "1.0.6"
+__version__ = "1.0.7"
 
 comfy_frontend = os.path.join(comfy_dir, 'web', 'extensions')
 frontend_target = os.path.join(comfy_frontend, 'Primere')
