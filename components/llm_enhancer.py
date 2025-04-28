4b62476867eaecddc182ce008a6f58d498f8630b
DESKTOP-CNFQ7PM\Primere <gitarfogas@gmail.com>
2025-04-28 12:09:03 +0200

    V 1.0.7 - LLM enhancer - adapter test #2

--- components/llm_enhancer.py
+++ components/llm_enhancer.py
@@ -28,10 +28,11 @@ class PromptEnhancerLLM:
             baseRepo = self.model_path[:self.model_path.lower().index("-promptenhancing")]
             print('---------------')
             print(baseRepo)
-            print('---------------')
             loraRepo = self.model_path
             model_access = os.path.join(ROOT_PATH, baseRepo)
             lora_access = os.path.join(ROOT_PATH, loraRepo)
+            print(lora_access)
+            print('---------------')
 
             self.tokenizer = AutoTokenizer.from_pretrained(model_access, clean_up_tokenization_spaces=False)
             self.model = AutoModelForCausalLM.from_pretrained(model_access, torch_dtype=torch.bfloat16).to(self.device)
