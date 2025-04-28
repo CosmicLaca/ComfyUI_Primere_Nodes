af7b6920f23da32a295a33ce58ca3033e0328a0c
DESKTOP-CNFQ7PM\Primere <gitarfogas@gmail.com>
2025-04-28 10:11:33 +0200

    V 1.0.7 - LLM enhancer - user prompt #2

--- components/llm_enhancer.py
+++ components/llm_enhancer.py
@@ -295,9 +295,9 @@ class PromptEnhancerLLM:
                 elif "lex-enhancer" in self.model_path.lower():
                     SYSTEM_TEMPLATE = (
                         "A conversation between User and Assistant. The user asks a question, and the Assistant solves it. "
-                        "The assistant first thinks about the reasoning process in the mind and then provides the user with the answer. "
-                        "The reasoning process and answer are enclosed within <think> </think> and <answer> </answer> tags, respectively, i.e., "
-                        "<think> reasoning process here </think> <answer> answer here </answer>."
+                        # "The assistant first thinks about the reasoning process in the mind and then provides the user with the answer. "
+                        # "The reasoning process and answer are enclosed within <think> </think> and <answer> </answer> tags, respectively, i.e., "
+                        # "<think> reasoning process here </think> <answer> answer here </answer>."
                     )
 
                     simple_caption = input_text #"A thank you card with the words very much, with the text on it: \"VERY\" in black, \"MUCH\" in yellow."
@@ -314,16 +314,16 @@ class PromptEnhancerLLM:
                     def create_user_prompt(simple_caption):
                         return (
                             "Below is the simple caption of an image with text. Please deduce the detailed description of the image based on this simple caption. "
-                            "Note: 1. The description should only include visual elements and should not contain any extended meanings. "
-                            "2. The visual elements should be as rich as possible, such as the main objects in the image, their respective attributes, "
-                            "the spatial relationships between the objects, lighting and shadows, color style, any text in the image and its style, etc. "
-                            "3. The output description should be a single paragraph and should not be structured. "
-                            "4. The description should avoid certain situations, such as pure white or black backgrounds, blurry text, excessive rendering of text, "
-                            "or harsh visual styles. "
-                            "5. The detailed caption should be human readable and fluent. "
-                            "6. Avoid using vague expressions such as \"may be\" or \"might be\"; the generated caption must be in a definitive, narrative tone. "
-                            "7. Do not use negative sentence structures, such as \"there is nothing in the image,\" etc. The entire caption should directly describe the content of the image. "
-                            "8. The entire output should be limited to 200 words."
+                            # "Note: 1. The description should only include visual elements and should not contain any extended meanings. "
+                            # "2. The visual elements should be as rich as possible, such as the main objects in the image, their respective attributes, "
+                            # "the spatial relationships between the objects, lighting and shadows, color style, any text in the image and its style, etc. "
+                            # "3. The output description should be a single paragraph and should not be structured. "
+                            # "4. The description should avoid certain situations, such as pure white or black backgrounds, blurry text, excessive rendering of text, "
+                            # "or harsh visual styles. "
+                            "The detailed caption should be human readable and fluent. "
+                            "Avoid using vague expressions such as \"may be\" or \"might be\"; the generated caption must be in a definitive, narrative tone. "
+                            "Do not use negative sentence structures, such as \"there is nothing in the image,\" etc. The entire caption should directly describe the content of the image. "
+                            "The entire output should be limited to 200 words."
                             f"SIMPLE CAPTION: {simple_caption}"
                         )
 
@@ -333,7 +333,7 @@ class PromptEnhancerLLM:
 
                     output = self.model.generate(
                         input_ids,
-                        max_length=2048,
+                        max_length=160,
                         num_return_sequences=1,
                         do_sample=True,
                         temperature=0.6,
