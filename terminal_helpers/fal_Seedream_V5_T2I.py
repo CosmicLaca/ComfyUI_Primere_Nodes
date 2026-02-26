de30a4f85e6843c5523fd4b9cf674fc5de084901
DESKTOP-TVBJISQ\Primere <primerecomfydev@gmail.com>
2026-02-26 09:43:41 +0100

    V 2.0.0 - Universal api #18 - FAL #1

--- terminal_helpers/fal_Seedream_V5_T2I.py
+++ terminal_helpers/fal_Seedream_V5_T2I.py
@@ -1,16 +1,14 @@
-handler = lient.submit("fal-ai/bytedance/seedream/v5/lite/edit",
+handler = client.submit("fal-ai/bytedance/seedream/v5/lite/text-to-image",
     arguments={
         "prompt": prompt,
-        "img_urls": [images],
         "image_size": {
           "width": 1280,
           "height": 720
         },
-        "num_images": batch,
+        "num_images": 1,
         "max_images": 1,
-        "enable_safety_checker": False,
+        "enable_safety_checker": true,
         "enhance_prompt_mode": "standard",
         "seed": seed
-    },
-    with_logs=False
+    }
 )
