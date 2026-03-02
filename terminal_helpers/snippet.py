037b3c92caced8ca423fd8851a7b390e93f16f5b
DESKTOP-TVBJISQ\Primere <primerecomfydev@gmail.com>
2026-03-02 11:01:30 +0100

    V 2.0.0 - Universal api #44 - OAI

--- terminal_helpers/snippet.py
+++ terminal_helpers/snippet.py
@@ -1,12 +1,19 @@
-response = client.images.generate(
-    model=model,
-    prompt=prompt,
-    n=batch,
-    moderation = moderation,
-    background = background,
-    output_format = output_format,
-    output_compression = "INT",
-    size=size,
-    quality = quality,
-    response_format = response_format
-)
+response = requests.post("https://api.bfl.ai/v1/flux-2-klein-4b",
+     headers={
+         "accept": "application/json",
+         "x-key": os.environ.get("BFL_API_KEY"),
+         "Content-Type": "application/json",
+     },
+     json={
+         "output_format": "png",
+         "image_prompt": reference_images,
+         "safety_tolerance": safety_tolerance,
+         "prompt": prompt,
+         "seed": seed,
+         "width": width,
+         "height": height,
+         "prompt_upsampling": "BOOLEAN",
+         "guidance": "FLOAT",
+         "steps": "INT",
+     },
+ )
